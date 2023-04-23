636bd34c9693d97cc6915dca703714773c652ece
elmansey <almanseyalmansey6@gmail.com>
2023-04-23 20:11:59 +0200

    edit in test.js

--- test.js
+++ test.js
@@ -4,14 +4,13 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('GET /', function() {
-  it('should respond with "Hello, world!"', function(done) {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end(function(err, res) {
-        if (err) return done(err);
-        expect(res.text).to.equal('Hello, world!');
-        done();
+    it('should respond with "Hello, world!"', function(done) {
+      var server = app.listen(3000, function() {
+        request(server)
+          .get('/')
+          .expect(200)
+          .expect("Hello, world!", done);
       });
+      server.close()
+    });
   });
-});
